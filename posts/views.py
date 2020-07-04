427e8f326387febf48ca61edcff8673c3ca96f98
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-07-04 07:48:30 +0600

    decorated

--- posts/views.py
+++ posts/views.py
@@ -2,16 +2,15 @@ from django.db.models import Count
 from django.core.paginator import Paginator
 
 from django.shortcuts import (render,
-                                get_object_or_404,
-                                redirect,
-                                reverse,)
-                            
+                              get_object_or_404,
+                              redirect,
+                              reverse,)
+
 from django.http import HttpResponseRedirect
-from django.views.generic import View,TemplateView,ListView,DetailView,CreateView
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView
 from django.contrib.auth.decorators import login_required
 from .models import Post, Category, Comment, Author
-from .forms import CommentForm,PostForm
-
+from .forms import CommentForm, PostForm
 
 
 # Create your views here.
@@ -22,10 +21,11 @@ def get_author(user):
         return qs[0]
     return None
 
-def get_category_count():
-    queryset = Post.objects.values('categories__title').annotate(Count('categories__title'))
-    return queryset
 
+def get_category_count():
+    queryset = Post.objects.values(
+        'categories__title').annotate(Count('categories__title'))
+    return queryset
 
 
 class IndexView(View):
@@ -41,7 +41,6 @@ class IndexView(View):
     #     context['recent'] =recent,
     #     context['category_count'] =category_count,
     #     return context
-
 
     def get(self, request, *args, **kwargs):
         post = Post.objects.all()
@@ -67,16 +66,16 @@ class CanolaOilList(ListView):
     context_object_name = 'object_list'
     template_name = 'posts/post-list.html'
     queryset = Post.objects.filter(categories__title="Canola Oil")
-    
 
     def get_context_data(self, **kwargs):
         category_count = get_category_count()
         recent = Post.objects.order_by('-timestamp')[0:10]
         context = super().get_context_data(**kwargs)
-        
+
         context['category_count'] = category_count
         context['recent'] = recent
         return context
+
 
 class CoconutOilList(ListView):
     context_object_name = 'object_list'
@@ -87,10 +86,11 @@ class CoconutOilList(ListView):
         category_count = get_category_count()
         recent = Post.objects.order_by('-timestamp')[0:10]
         context = super().get_context_data(**kwargs)
-        
+
         context['category_count'] = category_count
         context['recent'] = recent
         return context
+
 
 class CornOilList(ListView):
     context_object_name = 'object_list'
@@ -101,7 +101,7 @@ class CornOilList(ListView):
         category_count = get_category_count()
         recent = Post.objects.order_by('-timestamp')[0:10]
         context = super().get_context_data(**kwargs)
-        
+
         context['category_count'] = category_count
         context['recent'] = recent
         return context
@@ -116,10 +116,11 @@ class CottonseedOilList(ListView):
         category_count = get_category_count()
         recent = Post.objects.order_by('-timestamp')[0:10]
         context = super().get_context_data(**kwargs)
-        
+
         context['category_count'] = category_count
         context['recent'] = recent
         return context
+
 
 class OliveOilList(ListView):
     context_object_name = 'object_list'
@@ -130,10 +131,11 @@ class OliveOilList(ListView):
         category_count = get_category_count()
         recent = Post.objects.order_by('-timestamp')[0:10]
         context = super().get_context_data(**kwargs)
-        
+
         context['category_count'] = category_count
         context['recent'] = recent
         return context
+
 
 class PalmOilList(ListView):
     context_object_name = 'object_list'
@@ -144,10 +146,11 @@ class PalmOilList(ListView):
         category_count = get_category_count()
         recent = Post.objects.order_by('-timestamp')[0:10]
         context = super().get_context_data(**kwargs)
-        
+
         context['category_count'] = category_count
         context['recent'] = recent
         return context
+
 
 class PeanutOilList(ListView):
     context_object_name = 'object_list'
@@ -158,10 +161,11 @@ class PeanutOilList(ListView):
         category_count = get_category_count()
         recent = Post.objects.order_by('-timestamp')[0:10]
         context = super().get_context_data(**kwargs)
-        
+
         context['category_count'] = category_count
         context['recent'] = recent
         return context
+
 
 class RapeseedOilList(ListView):
     context_object_name = 'object_list'
@@ -172,10 +176,11 @@ class RapeseedOilList(ListView):
         category_count = get_category_count()
         recent = Post.objects.order_by('-timestamp')[0:10]
         context = super().get_context_data(**kwargs)
-        
+
         context['category_count'] = category_count
         context['recent'] = recent
         return context
+
 
 class SunflowerOilList(ListView):
     context_object_name = 'object_list'
@@ -187,12 +192,10 @@ class SunflowerOilList(ListView):
         category_count = get_category_count()
         recent = Post.objects.order_by('-timestamp')[0:10]
         context = super().get_context_data(**kwargs)
-        
+
         context['category_count'] = category_count
         context['recent'] = recent
         return context
-
-
 
 
 class PostDetailView(DetailView):
@@ -224,20 +227,19 @@ class PostDetailView(DetailView):
         context['total_likes'] = total_likes
         context['total_unlikes'] = total_unlikes
         context['liked'] = liked
-        return context 
-    
+        return context
 
-    def post(self, request,pk, **kwargs):
+    def post(self, request, pk, **kwargs):
         post = get_object_or_404(Post, pk=pk)
         if request.method == 'POST':
             form = CommentForm(request.POST)
             if form.is_valid():
                 comment = form.save(commit=False)
-                comment.post = post 
+                comment.post = post
                 comment.save()
-                
+
                 return redirect(reverse("post-detail", kwargs={
-                    'pk':post.pk
+                    'pk': post.pk
                 }))
         else:
             form = CommentForm()
@@ -245,7 +247,7 @@ class PostDetailView(DetailView):
 # class PostCategory(ListView):
 #     model = Post
 #     template_name = "posts/post-category.html"
-    
+
 #     def get_queryset(self, kwargs):
 #         category = get_object_or_404(Category, pk=self.kwargs['pk'])
 #         return Post.objects.filter(categories=category)
@@ -265,9 +267,7 @@ def LikeView(request, pk):
     else:
         post.likes.add(request.user)
         liked = True
-        
 
-    
     # unlike_post = get_object_or_404(Post, id=request.POST.get('post_unlike_id'))
 
     # liked = False
@@ -283,7 +283,7 @@ def LikeView(request, pk):
     #         liked = True
     #         unlike_post.unlikes.remove(request.user)
     #         unliked = False
-            
+
     #     else:
     #         post.likes.remove(request.user)
     #         liked = False
@@ -291,10 +291,4 @@ def LikeView(request, pk):
     # else:
     #     unlike_post.unlikes.remove(request.user)
 
-    
-    return HttpResponseRedirect(reverse("post-detail",args=[str(pk)]))
-
-
-
-
-
+    return HttpResponseRedirect(reverse("post-detail", args=[str(pk)]))
